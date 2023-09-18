eff594cfa2a19aeda30434e2864f0a0043050cf6
dungnguyenVitry <dunglannion@protonmail.com>
2023-09-18 20:16:05 +0200

    deploy back

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const userRoute = require("./routes/users");
+const pinRoute = require("./routes/pins");
+
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MongoDB Connected!")
+    })
+    .catch((err) => console.log(err));
+    
+app.use("/api/users", userRoute);
+
+app.use("/api/pins", pinRoute);
+
+
+const port = 8801;
+
+app.listen(port, () => {
+    console.log("Backend server is running")
+})
