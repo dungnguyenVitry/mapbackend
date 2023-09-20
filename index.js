ebce3a3b4bb94b4f92620b9a171ca95866f6cb7f
dungnguyenVitry <dunglannion@protonmail.com>
2023-09-20 08:55:37 +0200

    update link

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require('cors');
 const app = express();
 const userRoute = require("./routes/users");
 const pinRoute = require("./routes/pins");
@@ -8,6 +9,17 @@ const pinRoute = require("./routes/pins");
 dotenv.config();
 
 app.use(express.json());
+
+const allowedOrigins = ['https://travelpin-5ad2.onrender.com'];
+app.use(cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  }));
 
 mongoose
     .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
